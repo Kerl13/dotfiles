f6913c4d02e01c2827c9f528ed488eb87f8638fb
Martin Pépin <kerl@wkerl.me>
2018-09-13 09:16:43 +0200

    same fish conf on Carri & aji

---
+++ dotfiles/config/fish/functions/fish_prompt.fish
@@ -0,0 +1,13 @@
+function fish_prompt --description 'Write out the prompt'
+	if not set -q __fish_prompt_normal
+        set -g __fish_prompt_normal (set_color normal)
+    end
+
+    if not set -q __fish_prompt_cwd
+        set -g __fish_prompt_cwd (set_color $fish_color_cwd)
+    end
+
+    set_color c0430a
+
+    echo -n -s "$USER" @ (prompt_hostname) ' ' "$__fish_prompt_cwd" (prompt_pwd) (__fish_vcs_prompt) "$__fish_prompt_normal" '> '
+end
