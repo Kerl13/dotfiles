cd6fe75584fffcb0e3d33f64b8320207b54b455f
Martin Pépin <kerl@wkerl.me>
2018-10-05 00:21:37 +0200

    Avance un peu sur install.py

--- install.py
+++ install.py
@@ -1,52 +1,78 @@
+import argparse
 import os
+import sys
 
 
 HOME = os.environ.get("HOME")
-assert HOME != ""
-
-HERE = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)),
-    "dotfiles"
-)
+assert HOME
+HERE = os.path.join(os.path.dirname(os.path.abspath(__file__)), "dotfiles")
 
 
-def debug(msg):
-    print(f"[DEBUG] {msg}")
+class Link:
+    def __init__(self, source, target):
+        self.source = source
+        self.target = target
+
+    def __str__(self):
+        return f"ln -s {self.source} {self.target}"
+
+    def execute(self):
+        raise NotImplementedError()
 
 
-def fail(msg):
-    print(f"[error] #msg")
+class Ignore:
+    def __init__(self, file):
+        self.file = file
+
+    def __str__(self):
+        return f"Ignoring {self.file}"
+
+    def execute(self):
+        pass
 
 
-def link(filename, dest):
-    debug(f"ln -s {filename} {dest}")
+class Error:
+    def __init__(self, file):
+        self.file = file
+
+    def __str__(self):
+        return f"There is a problem with: {self.file}"
+
+    def execute(self):
+        print(str(self), file=sys.stderr)
 
 
-def install(location):
-    debug(f"Installing {location}")
-    for filename in os.listdir(location if location != "" else "."):
+def build_cmd_list(location, cmds):
+    real_location = location if location else "."
+    for filename in os.listdir(real_location):
         # Avoid hidden files like `.git`
         if filename.startswith("."):
-            debug(f"Ignoring {filename}")
             continue
         file = os.path.join(location, filename)
-        absfile = os.path.join(HERE, file)
-        destination = os.path.join(HOME, "." + file)
+        source = os.path.join(HERE, file)
+        target = os.path.join(HOME, "." + file)
 
-        if not os.path.exists(destination):
-            link(absfile, destination)
-        elif os.path.islink(destination):
-            debug(f"Already linked: {destination}")
-        elif os.path.isdir(destination) and os.path.isdir(file):
-            install(file)
+        if not os.path.exists(target):
+            cmds.append(Link(source, target))
+        elif os.path.islink(target):
+            cmds.append(Ignore(file))
+        elif os.path.isdir(target) and os.path.isdir(file):
+            build_cmd_list(file, cmds)
         else:
-            fail(f"I don't know what to do with {file}")
+            cmds.append(Error(file))
 
 
-def install_dir(dirname):
-    pass
+parser = argparse.ArgumentParser()
+parser.add_argument("--dry-run", action="store_const", const=True)
 
 
 if __name__ == "__main__":
+    config = parser.parse_args()
     os.chdir("dotfiles")
-    install("")
+    commands = []
+    build_cmd_list("", commands)
+    if config.dry_run:
+        for cmd in commands:
+            print(cmd)
+    else:
+        raise NotImplementedError()
