88ea1ab1340e472e5b8892f3a77a7f38fa08d562
Martin Pépin <kerl@wkerl.me>
2019-01-16 21:34:47 +0100

    proof-general arrête de faire nimps

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -22,7 +22,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (company-coq proof-general evil))))
+ '(package-selected-packages (quote (company-coq proof-general evil)))
+ '(proof-follow-mode (quote followdown))
+ '(proof-three-window-mode-policy (quote hybrid)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
