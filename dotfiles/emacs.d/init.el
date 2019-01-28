8ce23f76ff6ffeb38dbf62ad1ba43e4e4265164c
Martin Pépin <kerl@wkerl.me>
2019-01-28 19:28:52 +0100

    cider

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -22,9 +22,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (company-coq proof-general evil)))
  '(proof-follow-mode (quote followdown))
- '(proof-three-window-mode-policy (quote hybrid)))
+ '(proof-three-window-mode-policy (quote hybrid))
+ '(cider-auto-mode t)
+ '(package-selected-packages
+   (quote
+    (0blayout cider evil-smartparens company-coq proof-general evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
