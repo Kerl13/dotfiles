1f1877fe2e377a9f8d022c8370b422b478c87382
Martin Pépin <kerl@wkerl.me>
2019-02-05 20:04:37 +0100

    ipython vi bindings

---
+++ dotfiles/ipython/profile_default/ipython_config.py
@@ -0,0 +1,5 @@
+# Shortcut style to use at the prompt. 'vi' or 'emacs'.
+c.TerminalInteractiveShell.editing_mode = 'vi'
+
+# Set the editor used by IPython (default to $EDITOR/vi/notepad).
+c.TerminalInteractiveShell.editor = 'vim'
