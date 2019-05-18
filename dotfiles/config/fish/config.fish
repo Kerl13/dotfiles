7ff28195924f341af63d934a1efb1a3562b1b83e
Martin Pépin <kerl@wkerl.me>
2019-05-18 23:37:42 +0200

    Fix a weird opam issue (broke man)

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -7,7 +7,7 @@ end
 
 
 # OPAM configuration
-. /home/martin/.opam/opam-init/init.fish > /dev/null 2> /dev/null or true
+eval (opam env | grep -v MANPATH)
 
 # Add $HOME/.local/bin to PATH
 set -gx PATH "$HOME/.local/bin" $PATH
